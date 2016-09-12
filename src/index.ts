40d238f2948084a3a17d7abf1babc197a4869a32
Andre Staltz <andre@staltz.com>
2016-09-12 21:52:34 +0300

    Remove everything

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export {makeHistoryDriver} from './makeHistoryDriver';
-export {createServerHistory} from './serverHistory';
-export {supportsHistory, createLocation} from './util';
-
-export * from './interfaces';
